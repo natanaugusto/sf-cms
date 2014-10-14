8cc4d005c382c5302a940e71b6f66736202b3160
Natan Augusto <natanaugusto@gmail.com>
2014-10-14 15:47:51 -0300

    Alterações nas rotas do Dash

--- src/Cms/DashBundle/Controller/DefaultController.php
+++ src/Cms/DashBundle/Controller/DefaultController.php
@@ -3,11 +3,12 @@
 namespace Cms\DashBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('CmsDashBundle:Default:index.html.twig', array('name' => $name));
+        return new Response('Teste');
     }
 }
