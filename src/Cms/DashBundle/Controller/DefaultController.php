fe53f6b3862c4f01a0ea650852ebb809612819c5
Natan Augusto <natanaugusto@gmail.com>
2014-10-14 18:39:08 -0300

    ADD - Pagina inicial do Dash

--- src/Cms/DashBundle/Controller/DefaultController.php
+++ src/Cms/DashBundle/Controller/DefaultController.php
@@ -10,4 +10,9 @@ class DefaultController extends Controller
     {
         return $this->render('CmsDashBundle:Default:index.html.twig');
     }
+
+    public function boardAction()
+    {
+        return $this->render('CmsDashBundle:Default:board.html.twig');
+    }
 }
---
+++ src/Cms/DashBundle/Resources/public/js/main.js
@@ -0,0 +1,77 @@
+function LoadAjaxContent(url){
+	$('.preloader').show();
+	$.ajax({
+		mimeType: 'text/html; charset=utf-8', // ! Need set mimeType only when run from local file
+		url: url,
+		type: 'GET',
+		success: function(data) {
+			$('#ajax-content').html(data);
+			$('.preloader').hide();
+		},
+		error: function (jqXHR, textStatus, errorThrown) {
+			alert(errorThrown);
+		},
+		dataType: "html",
+		async: false
+	});
+}
+$(document).ready(function () {
+	$('.show-sidebar').on('click', function (e) {
+		e.preventDefault();
+		$('div#main').toggleClass('sidebar-show');
+		setTimeout(MessagesMenuWidth, 250);
+	});
+
+	var ajax_url = location.hash.replace(/^#/, '');
+
+	if (ajax_url.length < 1) {
+		ajax_url = 'board';
+	}
+
+	LoadAjaxContent(ajax_url);
+
+	$('.main-menu').on('click', 'a', function (e) {
+		var parents = $(this).parents('li');
+		var li = $(this).closest('li.dropdown');
+		var another_items = $('.main-menu li').not(parents);
+		another_items.find('a').removeClass('active');
+		another_items.find('a').removeClass('active-parent');
+		if ($(this).hasClass('dropdown-toggle') || $(this).closest('li').find('ul').length == 0) {
+			$(this).addClass('active-parent');
+			var current = $(this).next();
+			if (current.is(':visible')) {
+				li.find("ul.dropdown-menu").slideUp('fast');
+				li.find("ul.dropdown-menu a").removeClass('active')
+			}
+			else {
+				another_items.find("ul.dropdown-menu").slideUp('fast');
+				current.slideDown('fast');
+			}
+		}
+		else {
+			if (li.find('a.dropdown-toggle').hasClass('active-parent')) {
+				var pre = $(this).closest('ul.dropdown-menu');
+				pre.find("li.dropdown").not($(this).closest('li')).find('ul.dropdown-menu').slideUp('fast');
+			}
+		}
+		if ($(this).hasClass('active') == false) {
+			$(this).parents("ul.dropdown-menu").find('a').removeClass('active');
+			$(this).addClass('active')
+		}
+		if ($(this).hasClass('ajax-link')) {
+			e.preventDefault();
+			if ($(this).hasClass('add-full')) {
+				$('#content').addClass('full-content');
+			}
+			else {
+				$('#content').removeClass('full-content');
+			}
+			var url = $(this).attr('href');
+			window.location.hash = url;
+			LoadAjaxContent(url);
+		}
+		if ($(this).attr('href') == '#') {
+			e.preventDefault();
+		}
+	});
+});
